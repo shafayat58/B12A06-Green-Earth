94ab7c6a7a8f4aa31f4e4d0dc37eee8393a38887
Shafayat Hossain <shafayathossaincse@gmail.com>
2026-02-02 20:34:37 +0600

    add all plants api

--- script/index.js
+++ script/index.js
@@ -29,10 +29,63 @@ const DisplayloadCatagory = (categories) => {
 }
 
 
+const loadAllPlant = () => {
+  const url = 'https://openapi.programming-hero.com/api/plants'
+  fetch(url)
+    .then(res => res.json())
+    .then(data => DisplayloadAllPlant(data.plants
+    ))
+}
+
+// "plants": [
+//   {
+//     "id": 1,
+//     "image": "https://i.ibb.co.com/cSQdg7tf/mango-min.jpg",
+//     "name": "Mango Tree",
+//     "description": "A fast-growing tropical tree that produces delicious, juicy mangoes during summer. Its dense green canopy offers shade, while its sweet fruits are rich in vitamins and minerals.",
+//     "category": "Fruit Tree",
+//     "price": 500
+//   },
 
 
 
+ const DisplayloadAllPlant = (plants) => {
+   const cardContainer = document.getElementById("card-container")
+  cardContainer.innerHTML = "";
+  
 
+   plants.forEach(plant => {
+    
+     const div = document.createElement("div")
+     div.innerHTML =
+       `
+    <div class="card bg-base-100 w-96 shadow-sm ">
+    <div class="card-body ">
+      <figure>
+        <img src="${plant.image}" alt="${plant.name}" class="plant-img"  />
+       </figure>
+      <h2 class="card-title">${plant.name}</h2>
+      <p>${plant.description}</p>
+      <div class="flex justify-between items-center">
+       <div class="rounded-full bg-[#DCFCE7]">${plant.category}</div>
+         <div class="font-bold"><i class="fa-solid fa-bangladeshi-taka-sign"></i>
+         ${plant.price}</div>
+      </div>
+
+      <button class="btn btn-wide  font-bold rounded-full bg-[#15803D] text-white mx-auto  ">Add To Cart</button>
+    </div>
+
+   </div>
+    
+    `
+     cardContainer.appendChild(div)
+  })
+   
+    
+ }
 
 
 loadCatagory()
+loadAllPlant()
+
+
