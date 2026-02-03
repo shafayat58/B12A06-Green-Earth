37030a8c7bada5f014fd6a3c05f5367e133a7e29
Shafayat Hossain <shafayathossaincse@gmail.com>
2026-02-03 14:00:07 +0600

    add modal and plants detais

--- script/index.js
+++ script/index.js
@@ -8,16 +8,71 @@ const loadCatagory = () => {
   )
 }
 
-const loadFood = (id) => {
+const loadPlants = (id) => {
   const url = `https://openapi.programming-hero.com/api/category/${id}`
   fetch(url)
     .then(res => res.json())
-    .then(data => DisplayloadFood(data.plants)
+    .then(data => DisplayloadPlants(data.plants)
   )
   
 }
 
-const DisplayloadFood = (plants) => {
+
+const plantDetails = (id) => {
+  const url = `https://openapi.programming-hero.com/api/plant/${id}`
+  console.log(url);
+  fetch(url)
+    .then(res => res.json())
+    .then(data => DisplayplantDetails(data.plants)
+  )
+  
+  
+}
+
+
+
+
+
+const DisplayplantDetails = (plants) => {
+  const DetailsContainer = document.getElementById("details-container")
+  DetailsContainer.innerHTML = `
+     <div class="bg-white rounded-lg shadow-lg p-4 space-y-3">
+
+      <h2 class="text-xl font-bold">${plants.name}</h2>
+
+      <figure>
+        <img class="plant-img" src="${plants.image}" alt="${plants.name}"
+          class="w-full h-48 object-cover rounded-md" />
+      </figure>
+
+      <div>
+        <h3><span class="font-bold">Category:</span> ${plants.category}</h3>
+      </div>
+
+      <h3>
+        <span class="font-bold">Price:</span>
+        <i class="fa-solid fa-bangladeshi-taka-sign"></i> ${plants.price}
+      </h3>
+
+      <div>
+        <p><span class="font-bold">Description:</span> ${plants.description}</p>
+      </div>
+
+      <div class="modal-action">
+        <form method="dialog">
+          <button class="btn">Close</button>
+        </form>
+      </div>
+  `
+  document.getElementById("word_modal").showModal()
+}
+
+
+
+
+
+
+const DisplayloadPlants = (plants) => {
 
   const cardContainer = document.getElementById("card-container")
   cardContainer.innerHTML = "";
@@ -33,7 +88,7 @@ const DisplayloadFood = (plants) => {
       <figure>
         <img src="${plant.image}" alt="${plant.name}" class="plant-img"  />
        </figure>
-      <h2 class="card-title">${plant.name}</h2>
+      <h2 onclick="plantDetails(${plant.id})" class="card-title">${plant.name}</h2>
       <p>${plant.description}</p>
       <div class="flex justify-between items-center">
        <div class="rounded-full bg-[#DCFCE7]">${plant.category}</div>
@@ -62,7 +117,7 @@ const DisplayloadCatagory = (categories) => {
     const div = document.createElement("div")
     div.innerHTML =
       `
-<button onclick="loadFood(${categorie.id})" class="btn btn-wide text-2xl font-bold hover:bg-green-500 rounded bg-[#15803D] text-white px-4 py-2">${categorie.category_name}</button>
+<button onclick="loadPlants(${categorie.id})" class=" btn-wide text-2xl font-bold hover:bg-green-500 rounded  text-black px-4 py-2">${categorie.category_name}</button>
 
       `
     catagoryContainer.appendChild(div)
